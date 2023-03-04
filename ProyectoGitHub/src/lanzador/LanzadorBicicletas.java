5d2ba74794f7335baa69572f9a795c884b41e7cc
angel <angel@LAPTOP-IGOQS1AI>
2023-03-04 23:19:00 +0100

    VERSION 2

--- ProyectoGitHub/src/lanzador/LanzadorBicicletas.java
+++ ProyectoGitHub/src/lanzador/LanzadorBicicletas.java
@@ -7,7 +7,7 @@ public class LanzadorBicicletas {
 
 	public static void main(String[] args) {
 
-		BiciMTB miBici = new BiciMTB("TREK", "X Caliber 9", Suspensiones.DELANTERA, (byte) 1, true, 1350.50f, (short) 0,
+		BiciMTB miBici = new BiciMTB("TREK", "X Caliber 9", Suspensiones.DELANTERA, (byte) 1, true, 2000.50f, (short) 0,
 				false);
 		BiciMTB miBici2 = new BiciMTB("TREK", "X Caliber 9", Suspensiones.DOBLE, (byte) 1, true, 1500.50f, (short) 0,
 				false);
