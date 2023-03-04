d105359a5543cce0b28a7e1df8dcdbd653170c66
josem <josem@DESKTOP-ASPDV2G>
2023-03-05 00:04:45 +0100

    Version 3 Lanzador

--- ProyectoGitHub/src/lanzador/LanzadorBicicletas.java
+++ ProyectoGitHub/src/lanzador/LanzadorBicicletas.java
@@ -9,7 +9,7 @@ public class LanzadorBicicletas {
 
 		BiciMTB miBici = new BiciMTB("TREK", "X Caliber 9", Suspensiones.DELANTERA, (byte) 1, true, 2000.50f, (short) 0,
 				false);
-		BiciMTB miBici2 = new BiciMTB("TREK", "X Caliber 9", Suspensiones.DOBLE, (byte) 1, true, 1500.50f, (short) 0,
+		BiciMTB miBici2 = new BiciMTB("TREK", "X Caliber 9", Suspensiones.DOBLE, (byte) 1, true, 1500.50f, (short) 10,
 				false);
 		System.out.println(miBici.toString());
 		miBici.acelerar();
